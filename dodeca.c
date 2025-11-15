be4ffe98e64cdb366b8fe0ea5a235401a0dabed2
H6rdy <137615703+H6rdy@users.noreply.github.com>
2025-11-15 20:14:36 +0900

    Add files via upload

--- dodeca.c
+++ dodeca.c
@@ -14,46 +14,39 @@ float K1 = 40;
 
 float quality = 0.01;
 
-// 황금비 (Golden Ratio)
 #define PHI 1.618033988749895
 
-// 정십이면체 꼭짓점 20개
-float vertices[20][3] = {
-    // 정육면체 8개 꼭짓점 (±1, ±1, ±1)
+
+float vertex[20][3] = {
     { 1,  1,  1}, { 1,  1, -1}, { 1, -1,  1}, { 1, -1, -1},
     {-1,  1,  1}, {-1,  1, -1}, {-1, -1,  1}, {-1, -1, -1},
     
-    // XY 평면 4개 (0, ±1/φ, ±φ)
     { 0,  1.0/PHI,  PHI}, { 0,  1.0/PHI, -PHI},
     { 0, -1.0/PHI,  PHI}, { 0, -1.0/PHI, -PHI},
     
-    // YZ 평면 4개 (±1/φ, ±φ, 0)
     { 1.0/PHI,  PHI, 0}, {-1.0/PHI,  PHI, 0},
     { 1.0/PHI, -PHI, 0}, {-1.0/PHI, -PHI, 0},
     
-    // XZ 평면 4개 (±φ, 0, ±1/φ)
     { PHI, 0,  1.0/PHI}, { PHI, 0, -1.0/PHI},
     {-PHI, 0,  1.0/PHI}, {-PHI, 0, -1.0/PHI}
 };
 
-// 정십이면체 12개 면 (각 면은 정오각형, 5개 꼭짓점)
 int faces[12][5] = {
-    {0, 8, 10, 2, 16},    // 면0
-    {0, 16, 17, 1, 12},   // 면1
-    {0, 12, 13, 4, 8},    // 면2
-    {1, 17, 3, 11, 9},    // 면3
-    {1, 9, 5, 13, 12},    // 면4
-    {2, 10, 6, 15, 14},   // 면5
-    {2, 14, 3, 17, 16},   // 면6
-    {3, 14, 15, 7, 11},   // 면7
-    {4, 13, 5, 19, 18},   // 면8
-    {4, 18, 6, 10, 8},    // 면9
-    {5, 9, 11, 7, 19},    // 면10
-    {6, 18, 19, 7, 15}    // 면11
+    {0, 8, 10, 2, 16},   
+    {0, 16, 17, 1, 12},  
+    {0, 12, 13, 4, 8},   
+    {1, 17, 3, 11, 9},   
+    {1, 9, 5, 13, 12},   
+    {2, 10, 6, 15, 14},  
+    {2, 14, 3, 17, 16},  
+    {3, 14, 15, 7, 11},  
+    {4, 13, 5, 19, 18},  
+    {4, 18, 6, 10, 8},   
+    {5, 9, 11, 7, 19},    
+    {6, 18, 19, 7, 15}    
 };
 
-// 각 면의 문자
-char surChars[12] = {'@', 'r', '=', 'x', '*', '+', ':', '-', '$', '.', 'o', '~'};
+char surfaceChars[12] = {'@', 'r', '=', 'x', '*', '+', ':', '-', '$', '.', 'o', '~'};
 
 float x, y, z;
 float ooz;
@@ -94,7 +87,6 @@ void surface(float px, float py, float pz, int ch) {
   }
 }
 
-// 삼각형 렌더링 (정사면체와 동일)
 void triangle(float v1[3], float v2[3], float v3[3], char ch, float scale) {
   for (float u = 0; u < 1.0; u += quality) {
     for (float v = 0; v < 1.0 - u; v += quality) {
@@ -111,23 +103,19 @@ void triangle(float v1[3], float v2[3], float v3[3], char ch, float scale) {
   }
 }
 
-// 정오각형을 삼각형 3개로 분할하여 렌더링
 void pentagon(int faceIdx, float scale) {
   float v1[3], v2[3], v3[3], v4[3], v5[3];
   
-  // 5개 꼭짓점 가져오기
   for (int i = 0; i < 3; i++) {
-    v1[i] = vertices[faces[faceIdx][0]][i];
-    v2[i] = vertices[faces[faceIdx][1]][i];
-    v3[i] = vertices[faces[faceIdx][2]][i];
-    v4[i] = vertices[faces[faceIdx][3]][i];
-    v5[i] = vertices[faces[faceIdx][4]][i];
+    v1[i] = vertex[faces[faceIdx][0]][i];
+    v2[i] = vertex[faces[faceIdx][1]][i];
+    v3[i] = vertex[faces[faceIdx][2]][i];
+    v4[i] = vertex[faces[faceIdx][3]][i];
+    v5[i] = vertex[faces[faceIdx][4]][i];
   }
   
-  char ch = surChars[faceIdx];
+  char ch = surfaceChars[faceIdx];
   
-  // 정오각형을 3개 삼각형으로 분할
-  // 팬 삼각분할 (Fan Triangulation): v1을 중심으로
   triangle(v1, v2, v3, ch, scale);
   triangle(v1, v3, v4, ch, scale);
   triangle(v1, v4, v5, ch, scale);
@@ -143,7 +131,6 @@ int main() {
     memset(buffer, background, width * height);
     memset(zBuffer, 0, width * height * 4);
     
-    // 12개 면 렌더링
     for (int i = 0; i < 12; i++) {
       pentagon(i, scale);
     }
